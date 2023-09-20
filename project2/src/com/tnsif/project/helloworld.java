a5cbee0fdd6c6be38ca0b8621c75e88cbbade374
acer <acer@10.0.0.110>
2023-09-20 12:52:39 +0530

    First commit

---
+++ project2/src/com/tnsif/project/helloworld.java
@@ -0,0 +1,11 @@
+package com.tnsif.project;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printla("hello world");
+
+	}
+
+}
