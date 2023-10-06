dd7233febe849a5faf4adb9af4482bda94f1ab1d
acer <acer@TMP249-G2-M.bbrouter>
2023-10-06 16:36:02 +0530

    first commit

---
+++ project2/src/com/tnsif/project/Encapsulation.java
@@ -0,0 +1,16 @@
+package com.tnsif.project;
+
+public class Encapsulation {
+	//data members
+	public int serialno;
+	public String name;
+	public int age;
+	//member function 
+	public void show()
+	{
+		System.out.println("serialno"+serialno+"name"+age+"age");
+		
+	}
+	
+
+}
