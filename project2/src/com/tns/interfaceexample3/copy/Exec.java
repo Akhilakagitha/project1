15c96c88173a74b23ddad75fbc311ae7bb88594b
acer <acer@TMP249-G2-M.bbrouter>
2023-10-06 16:39:33 +0530

    first commit

---
+++ project2/src/com/tns/interfaceexample3/copy/Exec.java
@@ -0,0 +1,14 @@
+package com.tns.interfaceexample3.copy;
+
+import com.tns.interfaceexample1.Demo;
+
+public class Exec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Demo o=new Demo();
+        o.show();
+        o.print();
+	}
+
+}
